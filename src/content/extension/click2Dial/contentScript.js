006ff791becd8b24c66dd981a4f9369d987cbc8f
chrou <chrou@grandstream.cn>
2022-11-22 18:02:52 +0800

    update

--- src/content/extension/click2Dial/contentScript.js
+++ src/content/extension/click2Dial/contentScript.js
@@ -8,7 +8,21 @@ let contentIdentification = {
 		'AUDIO',
 		'VIDEO',
 		'CANVAS',
-		'SCRIPT'
+		'SCRIPT',
+		'STYLE',
+		'A',
+		'SCRIPT',
+		'IMG',
+		'TEXTAREA',
+		'INPUT',
+		'SELECT',
+		'PRE',
+		'CODE',
+		'STYLE',
+		'CANVAS',
+		'SVG',
+		'rect',
+		'clipPath',
 	],
 	phoneCallProtocolList: ['tel://', 'callto://', 'wavecallto://', 'tel:', 'callto:', 'wavecallto:'],
 	regex: /([(])?([+]?)[-()]*[ ]?([0-9]{1,4})([)])?[- ()]*([0-9]{1,4})[- ()\/]*([0-9]{1,4})[- ]*([0-9]{0,4})[- ]*([0-9]{0,4})[- ]*([0-9]{0,4})[- ]*([0-9]{0,4})($|\s|[;,()（）；\s])/g,
@@ -19,6 +33,56 @@ let contentIdentification = {
 		new RegExp('(3[0-1]|[0-2][0-9]|[1-9])[\\/.-](1[0-2]|0[1-9]|[1-9])[\\/.-](([1-9][0-9])?[0-9]{2})($|\\)|\\s)', 'g'),
 		new RegExp('(1[0-2]|0[1-9]|[1-9])[\\/.-](3[0-1]|[0-2][0-9]|[1-9])[\\/.-](([1-9][0-9])?[0-9]{2})($|\\)|\\s)', 'g'),
 	],
+
+	/**
+	 * 验证手机号，处理规则：
+	 * 1--以1为开头；
+	 * 2--第二位可为3,4,5,7,8,中的任意一位；
+	 * 3--最后以0-9的9个整数结尾。
+	 * @param poneInput
+	 * @returns {boolean}
+	 */
+	isPoneAvailable: function(poneInput) {
+		let res = /^[1][3,4,5,7,8][0-9]{9}$/
+		return res.test(poneInput)
+	},
+
+	/**
+	 * 判断是否为电话号码
+	 * @param tel
+	 * @returns {boolean}
+	 */
+	isTelAvailable: function (tel){
+		let reg = /^(([0\+]\d{2,3}-)?(0\d{2,3})-)(\d{7,8})(-(\d{3,}))?$/
+		return reg.test(tel)
+	},
+
+	/**
+	 * 验证邮箱
+	 * @param emailInput
+	 * @returns {boolean}
+	 */
+	isEmailAvailable: function (emailInput){
+		let reg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/
+		return reg.test(emailInput)
+	},
+
+	/**
+	 * 同时匹配号码和邮箱
+	 * @param str
+	 */
+	phoneAndEmailMatch: function (str){
+		const res = /(1[0-9]{2,10})|([0-9]{3,4})?[0-9]{7,8}|[\d\w]+\b@[a-zA-ZA-z0-9]+.[a-z]+/g; //匹配手机号或者固话,邮箱
+		str = str.replace(/\s|[(]|[)]|[（]|[）]|[-]*/g, ''); //去除字符串中所有空格、小括号和横杠
+		let result = str.match(res); //识别手机号或者固话（在字符串内检索指定的值，或找到一个或多个正则表达式的匹配）
+		if(result){
+			// for (let i = 0; i < result.length; i++) {
+			// 	let temp = result[i]
+			// 	str = str.replace(result[i], '<a href="tel:' + temp + '"  class="copy phone-num" style=" text-decoration: underline;color: #2878FF;">' + temp + '</a>');
+			// }
+		}
+		return result
+	},
 
 	/**
 	 * 需要忽略的页面
@@ -98,35 +162,50 @@ let contentIdentification = {
 		let tagChars = this.escapeHtmlTagChars(targetNode.nodeValue)
 		if (tagChars) {
 			if (this.regexURL.test(tagChars)) { 	// 忽略URL
-				console.log(tagChars + ' skipped: matches URL regex')
+				// console.log(tagChars + ' skipped: matches URL regex')
 				return;
 			}
 			if (this.regexIP.test(tagChars)) { 	// 忽略IP
-				console.log(tagChars + ' skipped: matches IP address regex')
+				// console.log(tagChars + ' skipped: matches IP address regex')
 				return;
 			}
 			for (const item of this.regexDate) {  // 忽略日期
 				if (item.test(tagChars)) {
-					console.log(tagChars + ' skipped: matches date regex')
+					// console.log(tagChars + ' skipped: matches date regex')
 					return;
 				}
 			}
 
-			tagChars = this.replaceNBSPChar(tagChars) // 不处理prompt alert confirm弹框
-			if (tagChars.indexOf('prompt(') > 0 || tagChars.indexOf('alert(') > 0 || tagChars.indexOf('confirm(') > 0) {
-				console.log(tagChars + ' skipped: contains prompt alert confirm')
-				return
+			let matchList = this.phoneAndEmailMatch(tagChars)  // 获取所有能够匹配到的号码和邮箱列表
+			if(matchList){
+				for (let i = 0; i < matchList.length; i++) {
+					let matchStr = matchList[i]
+					tagChars = tagChars.replace(matchStr, '<grpphone>' + btoa(unescape(encodeURIComponent(matchStr))) + '</grpphone>')
+					searchedElementList.push({ element: targetNode, newhtml: tagChars })
+				}
 			}
-			let number = ''
-			let actualNumber = ''
-			tagChars.replace(this.regex, (replacement) => {
-				number = replacement
-				actualNumber = this.formatNumber(replacement)
-			})
-			// console.log('tagChars ', tagChars, ' 转换为数字 ', actualNumber, 'with replacement ', number)
-			// TODO: 这里把目标字符串替换为加密字符串
-			tagChars = tagChars.replace(number, '<grpphone>' + btoa(unescape(encodeURIComponent(number))) + '</grpphone>')
-			searchedElementList.push({element: targetNode, newhtml: tagChars,})
+
+
+			// tagChars = this.replaceNBSPChar(tagChars)
+			// if(this.regex.test(tagChars)){
+			// 	// 不处理prompt alert confirm弹框
+			// 	if (tagChars.indexOf('prompt(') > 0 || tagChars.indexOf('alert(') > 0 || tagChars.indexOf('confirm(') > 0) {
+			// 		console.log(tagChars + ' skipped: contains prompt alert confirm')
+			// 		return
+			// 	}
+			// 	let number = ''
+			// 	let actualNumber = ''
+			// 	tagChars.replace(this.regex, (replacement) => {
+			// 		number = replacement
+			// 		actualNumber = this.formatNumber(replacement)
+			// 	})
+			// 	// console.log('tagChars ', tagChars, ' 转换为数字 ', actualNumber, 'with replacement ', number)
+			// 	// TODO: 这里把目标字符串替换为加密字符串
+			// 	tagChars = tagChars.replace(number, '<grpphone>' + btoa(unescape(encodeURIComponent(number))) + '</grpphone>')
+			// 	searchedElementList.push({ element: targetNode, newhtml: tagChars })
+			// }else {
+			// 	// 筛选出不需要处理的部分
+			// }
 		}
 	},
 
@@ -174,24 +253,33 @@ let contentIdentification = {
 		let text = ''
 		for (let j = 0; j < searchedElementList.length; j++) {
 			const searchedParentNode = searchedElementList[j].element.parentNode
+			// console.log('node:', node)
+			// console.log('text：', text)
+			// console.log('searchedParentNode:', searchedParentNode)
+
 			if ((node !== searchedParentNode && (text && node && (node.innerHTML = text),
 			searchedParentNode && (text = searchedParentNode.innerHTML),
 				(node = searchedParentNode)),
 				searchedElementList[j].element.data)
 			) {
+				// console.log('node 2222:', node)
+				// console.log('text 2222：', text)
+				// console.log('searchedParentNode 2222222:', searchedParentNode)
+
+
 				let n = searchedElementList[j].newhtml
-				console.log('newhtml：', n)
+				// console.log('newhtml：', n)
 
 				n = n.replace(new RegExp('(?:<grptemp>)(.*?)(?:</grptemp>)', 'gm'), (e, t) =>
 					decodeURIComponent(escape(atob(t)))
 				)
 
-				console.log('replace 1:', n)
+				// console.log('replace 1:', n)
 				n = n.replace(new RegExp('(?:<grpphone>)(.*?)(?:</grpphone>)', 'gm'), (e, t) => {
 					const n = decodeURIComponent(escape(atob(t)))
 					return This.replaceNBSPChar(n).replace(This.regex, (e) => This.phoneCallItem(e))
 				})
-				console.log('replace 2:', n)
+				// console.log('replace 2:', n)
 				divEle.innerHTML = text
 
 				let outerHTMLWithReplace = text
@@ -204,8 +292,8 @@ let contentIdentification = {
 
 				// 转义 HTML 标签字符
 				const escapeData = This.escapeHtmlTagChars(searchedElementList[j].element.data).replace(/\xa0/g, '&nbsp;')
-				console.log('outerHTMLWithReplace:', outerHTMLWithReplace)
-				console.log('escapeData:', escapeData)
+				// console.log('outerHTMLWithReplace:', outerHTMLWithReplace)
+				// console.log('escapeData:', escapeData)
 
 				let fromIndex = outerHTMLWithReplace.length
 				let position = outerHTMLWithReplace.lastIndexOf('</grpSpan>', fromIndex) // 先查找'</grpSpan>'结束标签的位置，从该位置再查找，防止在'<grpSpan>'标签里有相同的字符
@@ -216,45 +304,12 @@ let contentIdentification = {
 					let t = text.substring(o + escapeData.length)
 					text = e + n + t
 
-					console.log('e:', e)
-					console.log('t:', t)
-					console.log('text:', text)
+					// console.log('e:', e)
+					// console.log('t:', t)
+					// console.log('text:', text)
 				}
 			}
 		}
-	},
-
-	/**
-	 * 验证手机号，处理规则：
-	 * 1--以1为开头；
-	 * 2--第二位可为3,4,5,7,8,中的任意一位；
-	 * 3--最后以0-9的9个整数结尾。
-	 * @param poneInput
-	 * @returns {boolean}
-	 */
-	isPoneAvailable: function(poneInput) {
-		let res = /^[1][3,4,5,7,8][0-9]{9}$/
-		return res.test(poneInput)
-	},
-
-	/**
-	 * 判断是否为电话号码
-	 * @param tel
-	 * @returns {boolean}
-	 */
-	isTelAvailable: function (tel){
-		let reg = /^(([0\+]\d{2,3}-)?(0\d{2,3})-)(\d{7,8})(-(\d{3,}))?$/
-		return reg.test(tel)
-	},
-
-	/**
-	 * 验证邮箱
-	 * @param emailInput
-	 * @returns {boolean}
-	 */
-	isEmailAvailable: function (emailInput){
-		let reg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/
-		return reg.test(emailInput)
 	},
 
 	/**
@@ -263,22 +318,6 @@ let contentIdentification = {
 	 */
 	handleClick2DialNumber: function (selectedText){
 		console.log('handleClick2DialNumber:', selectedText)
-	},
-
-	/**
-	 * 同时匹配号码和邮箱
-	 * @param str
-	 */
-	regularMatch: function (str){
-		const res = /(1[0-9]{2,10})|([0-9]{3,4})?[0-9]{7,8}|[\d\w]+\b@[a-zA-ZA-z0-9]+.[a-z]+/g; //匹配手机号或者固话,邮箱
-		str = str.replace(/\s|[(]|[)]|[（]|[）]|[-]*/g, ''); //去除字符串中所有空格、小括号和横杠
-		let result = str.match(res); //识别手机号或者固话（在字符串内检索指定的值，或找到一个或多个正则表达式的匹配）
-		if(result){
-			for (let i = 0; i < result.length; i++) {
-				let temp = result[i]
-				str = str.replace(result[i], '<a href="tel:' + temp + '"  class="copy phone-num" style=" text-decoration: underline;color: #2878FF;">' + temp + '</a>');
-			}
-		}
 	},
 
 	/**
